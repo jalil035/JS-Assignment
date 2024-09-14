f331f1b3285b5a55fbea8955204270270354d869
Abdul Jalil Miah <159704176+jalil035@users.noreply.github.com>
2024-09-14 12:30:35 +0600

    Frist

---
+++ index.js
@@ -0,0 +1,57 @@
+
+//1. Difference Between The First and Second Argument.
+function calculateDifference(a, b){
+    return a - b;
+}
+console.log(calculateDifference(45, 25));
+
+
+
+//2. Is Number Odd
+function isOdd(num){
+    return num % 2 !== 0;
+}
+console.log(isOdd(100));
+
+
+
+//3. Smallest Number in the Array
+function findMin(small){
+    return Math.min(...small);
+}
+console.log(findMin([10, 18, 3, 9]));
+
+
+
+//4. Filter Even Number
+function filterEvenNumbers(even){
+    return even.filter(num => num % 2 === 0);
+}
+console.log(filterEvenNumbers([4, 7, 6, 3, 11, 18, 34]));
+
+
+
+//5. Array Sorted in Descending
+function sortArrayDescending(descend){
+    return descend.slice().sort((a, b) => b - a);
+}
+console.log(sortArrayDescending([4, 7, 6, 3, 11, 18, 34]));
+
+
+
+//6. First Letter Lowercased
+function lowercaseFirstLetter(low){
+    if(!low) return low;
+    return low.charAt(0).toLowerCase() + low.slice(1);
+}
+console.log(lowercaseFirstLetter("Ostad"))
+
+
+
+//7. Vowel Counter
+function countVowels(countV){
+    let vowel = "aeiouAEIOU";
+    return countV.split("").filter(char => vowel.includes(char)).length;
+}
+console.log(countVowels("Ostad is The Best Platform for Tech Learning"));
+
